ce2a7322dba838376c7ea6ec3d888ae5955fe99c
Duy Nguyen <nguyendanh.duy9@gmail.com>
2021-07-15 14:42:48 -0400

    final commit

--- script/app.js
+++ script/app.js
@@ -1,6 +1,10 @@
-const UILoadCalculate = document.querySelector("#loan-calculate");
+const UILoanCalculate = document.querySelector("#loan-calculate");
 const UILoanResult = document.querySelector("#loan-result");
 const UILoadingBar = document.querySelector(".loading-bar");
+
+const UILoanAmount = document.querySelector("#loan-amount");
+const UILoanInterest = document.querySelector("#loan-interest");
+const UILoanYear = document.querySelector("#loan-year");
 
 const UILoanCalculateSubmit = document.querySelector("#loan-submit");
 
@@ -12,9 +16,27 @@ UILoanCalculateSubmit.addEventListener("click", calculateLoan);
 function calculateLoan(e) {
   e.preventDefault();
   // loading bar for 5 seconds
-  loadingBar();
+  if (isValidInput()) {
+    loadingBar();
+    setTimeout(showResult, 2500);
+  } else {
+    console.log("need to enter value correctly");
+    failedInput();
+  }
+}
 
-  setTimeout(showResult, 500);
+function failedInput() {
+  UILoanCalculate.style.color = "red";
+  setTimeout(function () {
+    window.location.reload(false);
+  }, 1000);
+}
+function isValidInput() {
+  if (UILoanAmount.value < 1 || isNaN(UILoanAmount.value)) return false;
+  if (UILoanInterest.value < 1 || isNaN(UILoanInterest.value)) return false;
+  if (UILoanYear.value < 1 || isNaN(UILoanYear.value)) return false;
+
+  return true;
 }
 
 function loadingBar() {
@@ -33,14 +55,28 @@ function calculateResult() {
   let p = parseFloat(document.querySelector("#loan-amount").value);
   let r = parseFloat(document.querySelector("#loan-interest").value);
   let t = parseFloat(document.querySelector("#loan-year").value);
+  //
+  const UILoanMonthly = document.querySelector("#loan-monthly");
+  const UILoanTotalPayment = document.querySelector("#loan-payment-total");
+  const UILoanTotalInterest = document.querySelector("#loan-interest-total");
 
-  r = r / 100;
+  UILoanAmount.value = "";
+  UILoanInterest.value = "";
+  UILoanYear.value = "";
+
+  r = r / 100 / 12;
   t = t * 12;
 
-  const loanMonthlyPayment = (
-    p *
-    ((Math.pow(r, t) * (r - 1)) / (Math.pow(r, t) - 1))
-  ).toFixed(2);
+  const x = Math.pow(1 + r, t);
+  const monthly = (p * x * r) / (x - 1);
+
+  if (isFinite(monthly)) {
+    UILoanMonthly.value = `${monthly.toFixed(2)}`;
+    UILoanTotalPayment.value = `${(monthly * t).toFixed(2)}`;
+    UILoanTotalInterest.value = `${(monthly * t - p).toFixed(2)}`;
+  } else {
+    console.log("somthing is wrong");
+  }
 }
 
 function outputResult(result) {
